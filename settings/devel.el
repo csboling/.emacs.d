702756954d90a68e44e91c80531ee54ad9918f03
Sam Boling <charles.samuel.boling@gmail.com>
2016-04-09 16:24:00 -0400

    fixed git ssh settings for windows

--- settings/devel.el
+++ settings/devel.el
@@ -1,3 +1,5 @@
 (use-package magit
+  :ensure ssh-agency
   :init
-  (global-set-key (kbd "C-x g") 'magit-status))
+  (global-set-key (kbd "C-x g") 'magit-status)
+  (setenv "SSH_ASKPASS" "git-gui--askpass"))
